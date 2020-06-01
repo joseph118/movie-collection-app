2f876e487ff468a39e61aeff6ea3e91a66f022ae
Joseph118 <josephborg118@gmail.com>
2020-06-01 19:27:37 +0200

    Added movie model & mock data. Adjusted home structure Added movie service Removed angular app code

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
-
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, HomeRoutingModule]
 })
-export class HomeModule { }
+export class HomeModule {}
