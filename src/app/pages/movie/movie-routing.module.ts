560f467349d57ccb041c261f27a0bb8f4e5ae5a8
Joseph118 <josephborg118@gmail.com>
2020-06-01 18:57:04 +0200

    Updated movie routing

--- src/app/pages/movie/movie-routing.module.ts
+++ src/app/pages/movie/movie-routing.module.ts
@@ -1,27 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MovieListComponent} from './movie-list/movie-list.component';
-import {MovieDetailComponent} from './movie-detail/movie-detail.component';
-
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 const routes: Routes = [
-    {
-        path: 'list',
-        component: MovieListComponent
-    },
-    {
-        path: 'detail/:id',
-        component: MovieDetailComponent
-    },
-    {
-        path: '**',
-        redirectTo: 'detail',
-        pathMatch: 'full'
-    }
+  {
+    path: '',
+    component: MovieListComponent
+  },
+  {
+    path: 'detail/:id',
+    component: MovieDetailComponent
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class MovieRoutingModule { }
+export class MovieRoutingModule {}
