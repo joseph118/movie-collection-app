37bce04f5ba31920a235c326617941a7f8e2dc9b
Joseph118 <josephborg118@gmail.com>
2020-06-04 19:45:29 +0200

    Changed filters to checkboxes and added styling

--- src/app/pages/movies/movies.module.ts
+++ src/app/pages/movies/movies.module.ts
@@ -5,9 +5,10 @@ import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { SharedModule } from '../../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MoviesComponent, MovieDetailComponent],
-  imports: [CommonModule, MoviesRoutingModule, SharedModule]
+  imports: [CommonModule, MoviesRoutingModule, SharedModule, FormsModule]
 })
 export class MoviesModule {}
