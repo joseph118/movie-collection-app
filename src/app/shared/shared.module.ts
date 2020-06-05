1545cc750aaa3b3c9b84cf0d8ee68aea292bb169
Joseph118 <josephborg118@gmail.com>
2020-06-05 21:26:29 +0200

    Added loader element

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoCardComponent } from './video-card/video-card.component';
 import { RouterModule } from '@angular/router';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  declarations: [VideoCardComponent],
+  declarations: [VideoCardComponent, LoaderComponent],
   imports: [CommonModule, RouterModule],
-  exports: [VideoCardComponent]
+  exports: [VideoCardComponent, LoaderComponent]
 })
 export class SharedModule {}
